34ed015b2a3fb6003f3eb165c3f8e4788ce6e8c9
Nicolas Diovani <nicolasdiovanioliveira@gmail.com>
2025-07-07 14:47:52 -0300

    login

--- controller/script.js
+++ controller/script.js
@@ -3,23 +3,33 @@ function registrar(){
     var email = document.getElementById("registrar_email").value
     var senha = document.getElementById("registrar_senha").value
     
-    if( nome.length == 0 ){
-        alert( "O campo nome é obrigatório!")
-    }else{
-        if( price.length == 0 ){
-            price = 0.0
-        }
-        var ajax = new XMLHttpRequest()
+    var ajax = new XMLHttpRequest()
 
-        ajax.onreadystatechange = function(){
-            if( this.readyState == 4 & this.status == 201){
-                alert( "Usuario " + nome + " cadastrado!")
-                buscarProdutos()
-            }
+    ajax.onreadystatechange = function(){
+        if( this.readyState == 4 & this.status == 201){
+            alert( "Usuario " + nome + " cadastrado!")
         }
-        ajax.open("POST" , "http://localhost:8001/registrar")
-        ajax.setRequestHeader("Content-type" , 
-                "application/x-www-form-urlencoded")
-        ajax.send("nome=" + nome + "&email=" + email + "&senha=" + senha)
     }
+    ajax.open("POST" , "http://localhost:8001/registrar")
+    ajax.setRequestHeader("Content-type" , 
+            "application/x-www-form-urlencoded")
+    ajax.send("usuario=" + nome + "&email=" + email + "&senha=" + senha)
+}
+function login(){
+    var usuario = document.getElementById("login_usuario").value
+    var senha = document.getElementById("login_senha").value
+    
+    var ajax = new XMLHttpRequest()
+
+    ajax.onreadystatechange = function(){
+        if( this.readyState == 4 & this.status == 200){
+            alert( "Usuario " + usuario + " logado!")
+        } else if (this.readyState == 4 & this.status == 401) {
+            alert("Usuario ou senha invalidos")
+        }
+    }
+    ajax.open("POST" , "http://localhost:8001/login")
+    ajax.setRequestHeader("Content-type" , 
+            "application/x-www-form-urlencoded")
+    ajax.send("usuario=" + usuario + "&senha=" + senha)
 }
