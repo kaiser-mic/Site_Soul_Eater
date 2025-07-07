ed4d61dfa11908d43cd8c1cfc6fb32b49d445f25
Nicolas Diovani <nicolasdiovanioliveira@gmail.com>
2025-07-07 15:17:44 -0300

    login 3

--- controller/script.js
+++ controller/script.js
@@ -15,21 +15,38 @@ function registrar(){
             "application/x-www-form-urlencoded")
     ajax.send("usuario=" + nome + "&email=" + email + "&senha=" + senha)
 }
-function login(){
-    var usuario = document.getElementById("login_nome").value
-    var senha = document.getElementById("login_senha").value
-    
-    var ajax = new XMLHttpRequest()
+function login() {
+    var usuario = document.getElementById("login_nome").value;
+    var senha = document.getElementById("login_senha").value;
 
-    ajax.onreadystatechange = function(){
-        if( this.readyState == 4 & this.status == 200){
-            alert( "Usuario " + usuario + " logado!")
-        } else if (this.readyState == 4 & this.status == 401) {
-            alert("Usuario ou senha invalidos")
-        }
+    if (!usuario || !senha) {
+        alert("Usuário e senha são necessários!");
+        return;
     }
-    ajax.open("POST" , "http://localhost:8001/login")
-    ajax.setRequestHeader("Content-type" , 
-            "application/x-www-form-urlencoded")
-    ajax.send("usuario=" + usuario + "&senha=" + senha)
+    
+    var ajax = new XMLHttpRequest();
+
+    ajax.onreadystatechange = function() {
+        if (this.readyState == 4) {
+            // Primeiro, tentamos decodificar a resposta como JSON
+            const resposta = JSON.parse(this.responseText);
+
+            if (this.status == 200) { // 200: OK (Sucesso)
+                alert(resposta.message); // "Login bem-sucedido"
+
+                // **NOVO E ESSENCIAL:** Salva o token no localStorage
+                localStorage.setItem('authToken', resposta.token);
+                
+                // Agora que o usuário está logado, podemos atualizar a interface
+                atualizarUI();
+
+            } else { // Trata qualquer outro status como erro (401, 500, etc.)
+                alert("Erro: " + resposta.error);
+            }
+        }
+    };
+
+    ajax.open("POST", "http://localhost:8001/login", true);
+    ajax.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    ajax.send("usuario=" + usuario + "&senha=" + senha);
 }
